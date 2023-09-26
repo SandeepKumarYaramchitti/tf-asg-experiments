bd1e0da44aff65390fdeb011b9539bdc0af49c52
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-09-26 07:31:49 -0400

    allow 8080 port

--- main.tf
+++ main.tf
@@ -116,6 +116,15 @@ resource "aws_security_group" "terramino_instance" {
   }
 
   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+
+  ingress {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
@@ -140,6 +149,8 @@ resource "aws_security_group" "terramino_lb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+
 
   egress {
     from_port   = 0
